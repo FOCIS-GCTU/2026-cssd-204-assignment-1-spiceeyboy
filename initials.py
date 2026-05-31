84d5a0881f785e7a0bf4efe980f9fa7178029091
spiceyboy <spiceyboy2003@gmail.com>
2026-05-31 10:35:01 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,6 +1,38 @@
+# File: initials.py
+# Description: Prints my initials in stylized large block letters
+# Assignment Number: 1
+#
+# Name: OWUSU ABRAH ISAAC
+# Student ID: 2425400589
+# Email: 2425400589@live.gctu.edu.gh
+# Grader: Augustus
+
+# On my honor, OWUSU ISAAC, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # This function prints the initials OAI in standard format and large block letters.
+    #Starting blank line
+    print()
+
+    #Small initials
+    print('...OAI')
+
+    #Blank line after small initials
+    print()
+
+    # Large initials
+    print("...OOOOOOOOOOOO......AAAAAAAAAAAA......IIIIIIIIIIII...")
+    print("...OOOOOOOOOOOO......AAAAAAAAAAAA......IIIIIIIIIIII...")
+    print("...OO........OO......AA........AA............II......")
+    print("...OO........OO......AA........AA............II......")
+    print("...OO........OO......AAAAAAAAAAAA............II......")
+    print("...OO........OO......AAAAAAAAAAAA............II......")
+    print("...OO........OO......AA........AA............II......")
+    print("...OO........OO......AA........AA............II......")
+    print("...OOOOOOOOOOOO..**..AA........AA..**..IIIIIIIIIIII..**")
+    print("...OOOOOOOOOOOO..**..AA........AA..**..IIIIIIIIIIII..**")
 
 
 
